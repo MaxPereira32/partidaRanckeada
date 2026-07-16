583f75d4d2c18e3a2b22cdd4c461f8faded10c0a
Max <lindomaxpereira@gmail.com>
2026-07-16 20:54:14 -0300

    Calculadora de partidas rankeadas

---
+++ desafio.js
@@ -0,0 +1,34 @@
+
+
+function partidaRanckeada(saldoVitorias) {
+    if (saldoVitorias <= 10) {
+        return "Ferro";
+    } else if (saldoVitorias <= 20) {
+        return "Bronze";
+    } else if (saldoVitorias <= 50) {
+        return "Prata";
+    } else if (saldoVitorias <= 80) {
+        return "Ouro";
+    } else if (saldoVitorias <= 90) {
+        return "Diamante";
+    } else if (saldoVitorias <= 100) {
+        return "Lendário";
+    } else {
+        return "Imortal";
+    }
+}
+
+let saldoVitorias = 0;
+let nivel = "";
+
+while (nivel !== "Imortal") {
+    let vitorias = Math.floor(Math.random() * 20) + 1;
+    let derrotas = Math.floor(Math.random() * 10) + 1;
+
+    saldoVitorias += vitorias - derrotas;
+    nivel = partidaRanckeada(saldoVitorias);
+
+    console.log(`+${vitorias} vitórias / -${derrotas} derrotas | Saldo: ${saldoVitorias} | Nível: ${nivel}`);
+}
+
+console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
